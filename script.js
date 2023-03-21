40253aba5d39c9294c1f9cd3fdeaa8706683d6b6
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-03-21 15:24:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function palindrome(str){
 	str.toLowerCase();
   let start=0;
    let end=str.length-1;
-	while(start<end){
+	while(start<=end){
 		if(str.charAt(start)!=str.charAt(end)){
 			return false;
 		}
