658def4da39b270dcf8e4cccb4f9a6ba15daf42f
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-04 16:36:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// complete the given function
-
 function palindrome(str){
-	str.toLowerCase();
-  let start=0;
-   let end=str.length-1;
-	while(start<=end){
-		if(str.charAt(start)!=str.charAt(end)){
-			return false;
-		}
-		start++;
-		end--;
-		
-	}
-	return true;
 	
+ str.toLowerCase();
+ const string = str.toLowerCase();
+ const len = str.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        if (string[i] !== string[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
 module.exports = palindrome
